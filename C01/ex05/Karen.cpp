877bffa376b1ecd862c5de51513bfa7e307395b5
Simone Menna <smenna@e3r3p2.42roma.it>
2021-09-24 13:24:29 +0200

    ex05 finished

---
+++ C01/ex05/Karen.cpp
@@ -0,0 +1,52 @@
+#include "Karen.hpp"
+
+Karen::Karen()
+{
+	this->levels[0] = "DEBUG";
+	this->levels[1] = "INFO";
+	this->levels[2] = "WARNING";
+	this->levels[3] = "ERROR";
+	this->func[0] = &Karen::debug;
+	this->func[1] = &Karen::info;
+	this->func[2] = &Karen::warning;
+	this->func[3] = &Karen::error;
+	return ;
+}
+
+Karen::~Karen()
+{
+	return ;
+}
+
+void Karen::debug()
+{
+	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << std::endl;
+}
+
+void Karen::info()
+{
+	std::cout << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!" << std::endl;
+}
+
+void Karen::warning()
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month." << std::endl;
+}
+
+void Karen::error()
+{
+	std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
+}
+
+void Karen::complain(std::string level)
+{
+	int i = 0;
+	while(i != 4)
+	{
+		if(!level.compare(levels[i]))
+			break ;
+		i++;
+	}
+	if(i != 4)
+		(this->*func[i])();
+}
---
+++ C01/ex05/main.cpp
@@ -0,0 +1,8 @@
+#include "Karen.hpp"
+
+int main()
+{
+    Karen myKaren;
+    myKaren.complain("debug");
+    return 0;
+}
