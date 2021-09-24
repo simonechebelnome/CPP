865aae347a2fa9ca5582210321e90ad0c91e90a9
Simone Menna <smenna@e4r8p4.42roma.it>
2021-09-24 14:45:31 +0200

    C00 has been updated

--- C00/ex01/Contact.cpp
+++ C00/ex01/Contact.cpp
@@ -1,49 +1,110 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Contact.cpp                                        :+:      :+:    :+:   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smenna <smenna@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 14:08:15 by smenna            #+#    #+#             */
-/*   Updated: 2021/09/24 14:08:16 by smenna           ###   ########.fr       */
+/*   Updated: 2021/09/24 14:41:26 by smenna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.hpp"
 
+void Contact::set_first(std::string first)
+{
+    this->_first_name = first;
+    return ;
+}
+
+void Contact::set_last(std::string last)
+{
+    this->_last_name = last;
+    return ;
+}
+
+void Contact::set_number(std::string number)
+{
+    this->_phone_number = number;
+    return ;
+}
+
+void Contact::set_nick(std::string nick)
+{
+    this->_first_name = nick;
+    return ;
+}
+
+void Contact::set_secret(std::string secret)
+{
+    this->_first_name = secret;
+    return ;
+}
+
+std::string Contact::get_first()
+{
+    return this->_first_name;
+}
+
+std::string Contact::get_last()
+{
+    return this->_last_name;
+}
+
+std::string Contact::get_nick()
+{
+    return this->_nickname;
+}
+
+std::string Contact::get_secret()
+{
+    return this->_darkest_secret;
+}
+
+std::string Contact::get_number()
+{
+    return this->_phone_number;
+}
+
 void PhoneBook::add_contact_info(int i)
 {
+    std::string temp;
     std::cout << "\e[0;35mFirst name: \e[0;32m";
-    std::cin >> this->person[i].first_name;
+    std::cin >> temp;
+    this->person[i].set_first(temp);
     std::cout << "\e[0;35mLast name: \e[0;32m";
-    std::cin >> this->person[i].last_name;
+    std::cin >> temp;
+    this->person[i].set_last(temp);
     std::cout << "\e[0;35mNickname: \e[0;32m";
-    std::cin >> this->person[i].nickname;
+    std::cin >> temp;
+    this->person[i].set_nick(temp);
     std::cout << "\e[0;35mPhone Number: \e[0;32m";
-    std::cin >> this->person[i].phone_number;
+    std::cin >> temp;
+    this->person[i].set_number(temp);
     std::cout << "\e[0;35mDarkest Secret: \e[0;32m";
-    std::cin >> this->person[i].darkest_secret;
+    std::cin >> temp;
+    this->person[i].set_secret(temp);
 }
 
 void PhoneBook::print_info(int i)
 {
-    std::cout << std::setw(16) << "\e[1;36mFirst Name: \e[0;32m" << this->person[i].first_name << std::endl;
-    std::cout << std::setw(16) << "\e[1;36mLast Name: \e[0;32m" << this->person[i].last_name << std::endl;
-    std::cout << std::setw(16) << "\e[1;36mNickname: \e[0;32m" << this->person[i].nickname << std::endl;
-    std::cout << std::setw(16) << "\e[1;36mPhone Number: \e[0;32m" << this->person[i].phone_number << std::endl;
-    std::cout << std::setw(16) << "\e[1;36mDarkest Secret: \e[0;32m" << this->person[i].darkest_secret << std::endl;
+    std::cout << std::setw(16) << "\e[1;36mFirst Name: \e[0;32m" << this->person[i].get_first() << std::endl;
+    std::cout << std::setw(16) << "\e[1;36mLast Name: \e[0;32m" << this->person[i].get_last() << std::endl;
+    std::cout << std::setw(16) << "\e[1;36mNickname: \e[0;32m" << this->person[i].get_nick() << std::endl;
+    std::cout << std::setw(16) << "\e[1;36mPhone Number: \e[0;32m" << this->person[i].get_number() << std::endl;
+    std::cout << std::setw(16) << "\e[1;36mDarkest Secret: \e[0;32m" << this->person[i].get_secret() << std::endl;
 }
 
 void PhoneBook::print_data(int i)
 {
     std::cout << "\e[0;32m|" << std::setw(10) << i + 1;
     std::cout << "\e[0;32m|\e[0m";
-	std::cout << std::setw(10) << str_trunc(this->person[i].first_name);
+	std::cout << std::setw(10) << str_trunc(this->person[i].get_first());
     std::cout << "\e[0;32m|\e[0m";
-	std::cout << std::setw(10) << str_trunc(this->person[i].last_name);
+	std::cout << std::setw(10) << str_trunc(this->person[i].get_last());
     std::cout << "\e[0;32m|\e[0m";
-	std::cout << std::setw(10) << str_trunc(this->person[i].nickname);
+	std::cout << std::setw(10) << str_trunc(this->person[i].get_nick());
     std::cout << "\e[0;32m|\e[0m";
     std::cout << std::endl;
 }
--- C00/ex01/Contact.hpp
+++ C00/ex01/Contact.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smenna <smenna@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 14:08:17 by smenna            #+#    #+#             */
-/*   Updated: 2021/09/24 14:08:18 by smenna           ###   ########.fr       */
+/*   Updated: 2021/09/24 14:41:05 by smenna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,23 @@
 class Contact
 {
 	public:
-		//Contact(/* args */);
-		//~Contact();
-		std::string	first_name;
-		std::string	last_name;
-		std::string	nickname;
-		std::string	phone_number;
-		std::string	darkest_secret;
+		void set_first(std::string first);
+		void set_last(std::string last);
+		void set_nick(std::string nick);
+		void set_number(std::string number);
+		void set_secret(std::string secret);
+		std::string get_first();
+		std::string get_last();
+		std::string get_nick();
+		std::string get_number();
+		std::string get_secret();
+		
+	private:
+		std::string	_first_name;
+		std::string	_last_name;
+		std::string	_nickname;
+		std::string	_phone_number;
+		std::string	_darkest_secret;
 };
 
 class PhoneBook
